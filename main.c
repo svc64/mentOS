d0d2e6b50350ec7b110aa42e3ab017a101e738c3
svc64 <svcx80@icloud.com>
2022-04-04 18:58:41 +0300

    prepare functions for FatFs

--- main.c
+++ main.c
@@ -14,21 +14,19 @@ void main() {
     // initialize the proc struct list
     proc_init();
     disable_irqs();
+    print("mentOS\n-------\n");
+    print("main addr: 0x%x\n", &main);
     print("_end: 0x%x\n", &_end);
-    print("mentOS\n");
     uint64_t current_el;
     __asm__ __volatile__("mrs %0, CurrentEL\n\t" : "=r" (current_el) :  : "memory");
     current_el = (current_el >> 2) & 0x3;
     print("Running in EL%d\n", current_el);
-
     // Set exception vectors
     __asm__ __volatile__("msr vbar_el1, %0\n\t; isb" : : "r" (&exception_vectors) : "memory");
     // do something stupid, just to make an exception
     // totally random constant, btw
     //uint64_t *wut = (uint64_t *)0x5be3749d0211f909;
     //*(uint64_t *)wut = 0xc0885663c55641d8;
-    print("main addr: 0x%x\n", &main);
-    print("proc_init()...\n");
     // create a new process
     int pid0 = proc_new((uintptr_t)&test_proc_1);
     print("created pid %d\n", pid0);
